addff1bba2020abc68649bcc84a13772d19cacd5
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-03-18 10:26:35 -0300

    add dotenv

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//Conexão com o banco de dados Mongo
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('DB connected'))
+    .catch(error => console.log(error));
 
 
 
-app.listen(5000, () => {
+
+
+//Porta
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!")
 });
