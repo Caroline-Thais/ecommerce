e54f8da15173d068b360bea600ba6b2be130f50b
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-03-18 13:43:15 -0300

    add crypto-js to hash password

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+
 
 dotenv.config();
 
@@ -12,7 +14,9 @@ mongoose
     .then(() => console.log('DB connected'))
     .catch(error => console.log(error));
 
-app.use('/api/user', userRoute);
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 
 //Porta
